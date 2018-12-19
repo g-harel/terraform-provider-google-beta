582d655eca13c9fc3587a1a33eb318080c5f4b6b
David Harrison <david.harrison@ladbiblegroup.com>
2018-12-19 22:46:36 +0000

    add create job method and supported functionality
    tested locally
    tested terraform apply against google and fails

--- google-beta/resource_cloudscheduler_job.go
+++ google-beta/resource_cloudscheduler_job.go
@@ -1,8 +1,14 @@
 package google
 
 import (
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
+
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
+	cloudscheduler "google.golang.org/api/cloudscheduler/v1beta1"
 )
 
 /*
@@ -20,6 +26,41 @@ TODO
 
 */
 
+type jobId struct {
+	Project string
+	Region  string
+	Name    string
+}
+
+func (s *jobId) jobId() string {
+	return fmt.Sprintf("projects/%s/locations/%s/jobs/%s", s.Project, s.Region, s.Name)
+}
+
+func (s *jobId) locationId() string {
+	return fmt.Sprintf("projects/%s/locations/%s", s.Project, s.Region)
+}
+
+func (s *jobId) terraformId() string {
+	return fmt.Sprintf("%s/%s/%s", s.Project, s.Region, s.Name)
+}
+
+func parseJobId(id string, config *Config) (*jobId, error) {
+	parts := strings.Split(id, "/")
+
+	cloudFuncIdRegex := regexp.MustCompile("^([a-z0-9-]+)/([a-z0-9-])+/([a-zA-Z0-9_-]{1,63})$")
+
+	if cloudFuncIdRegex.MatchString(id) {
+		return &jobId{
+			Project: parts[0],
+			Region:  parts[1],
+			Name:    parts[2],
+		}, nil
+	}
+
+	return nil, fmt.Errorf("Invalid Job id format, expecting " +
+		"`{projectId}/{regionId}/{jobName}`")
+}
+
 func resourceCloudSchedulerJob() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceCloudSchedulerJobCreate,
@@ -30,24 +71,29 @@ func resourceCloudSchedulerJob() *schema.Resource {
 		Schema: map[string]*schema.Schema{
 			"name": {
 				Type:     schema.TypeString,
+				ForceNew: true,
 				Required: true,
 			},
 			"description": {
 				Type:         schema.TypeString,
 				Optional:     true,
+				ForceNew:     true,
 				ValidateFunc: validation.StringLenBetween(0, 500),
 			},
 			"schedule": {
 				Type:     schema.TypeString,
+				ForceNew: true,
 				Required: true,
 			},
 			"timezone": {
 				Type:     schema.TypeString,
 				Optional: true,
+				ForceNew: true,
 			},
 			"retry_config": {
 				Type:     schema.TypeList,
 				Optional: true,
+				ForceNew: true,
 				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
@@ -78,6 +124,7 @@ func resourceCloudSchedulerJob() *schema.Resource {
 			"pubsub_target": {
 				Type:          schema.TypeList,
 				Optional:      true,
+				ForceNew:      true,
 				MaxItems:      1,
 				ConflictsWith: []string{"app_engine_http_target", "http_target"},
 				Elem: &schema.Resource{
@@ -100,6 +147,7 @@ func resourceCloudSchedulerJob() *schema.Resource {
 			"app_engine_http_target": {
 				Type:          schema.TypeList,
 				Optional:      true,
+				ForceNew:      true,
 				MaxItems:      1,
 				ConflictsWith: []string{"pubsub_target", "http_target"},
 				Elem: &schema.Resource{
@@ -147,6 +195,7 @@ func resourceCloudSchedulerJob() *schema.Resource {
 			"http_target": {
 				Type:          schema.TypeList,
 				Optional:      true,
+				ForceNew:      true,
 				MaxItems:      1,
 				ConflictsWith: []string{"pubsub_target", "app_engine_http_target"},
 				Elem: &schema.Resource{
@@ -170,11 +219,288 @@ func resourceCloudSchedulerJob() *schema.Resource {
 					},
 				},
 			},
+
+			"project": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
 		},
 	}
 }
 
+func expandPubsubTarget(configured []interface{}) *cloudscheduler.PubsubTarget {
+	if len(configured) == 0 || configured[0] == nil {
+		return &cloudscheduler.PubsubTarget{}
+	}
+
+	data := configured[0].(map[string]interface{})
+
+	attributes := make(map[string]string)
+	for k, val := range data["attributes"].(map[string]interface{}) {
+		attributes[k] = val.(string)
+	}
+
+	return &cloudscheduler.PubsubTarget{
+		Attributes: attributes,
+		Data:       data["data"].(string),
+		TopicName:  data["topic_name"].(string),
+	}
+}
+
+func flattenPubsubTarget(pubsubTarget *cloudscheduler.PubsubTarget) []map[string]interface{} {
+	result := make([]map[string]interface{}, 0, 1)
+	if pubsubTarget == nil {
+		return nil
+	}
+
+	result = append(result, map[string]interface{}{
+		"attributes": pubsubTarget.Attributes,
+		"data":       pubsubTarget.Data,
+		"topic_name": pubsubTarget.TopicName,
+	})
+
+	return result
+}
+
+func expandAppEngineRouting(configured []interface{}) *cloudscheduler.AppEngineRouting {
+	if len(configured) == 0 || configured[0] == nil {
+		return &cloudscheduler.AppEngineRouting{}
+	}
+
+	data := configured[0].(map[string]interface{})
+	return &cloudscheduler.AppEngineRouting{
+		Host:     data["host"].(string),
+		Instance: data["instance"].(string),
+		Service:  data["service"].(string),
+		Version:  data["version"].(string),
+	}
+}
+
+func flattenAppEngineRouting(appEngineRouting *cloudscheduler.AppEngineRouting) []map[string]interface{} {
+	result := make([]map[string]interface{}, 0, 1)
+	if appEngineRouting == nil {
+		return nil
+	}
+
+	result = append(result, map[string]interface{}{
+		"host":     appEngineRouting.Host,
+		"instance": appEngineRouting.Instance,
+		"service":  appEngineRouting.Service,
+		"version":  appEngineRouting.Version,
+	})
+
+	return result
+}
+
+func expandAppEngineHttpTarget(configured []interface{}) *cloudscheduler.AppEngineHttpTarget {
+	if len(configured) == 0 || configured[0] == nil {
+		return &cloudscheduler.AppEngineHttpTarget{}
+	}
+
+	data := configured[0].(map[string]interface{})
+
+	headers := make(map[string]string)
+	for k, val := range data["headers"].(map[string]interface{}) {
+		headers[k] = val.(string)
+	}
+	return &cloudscheduler.AppEngineHttpTarget{
+		Body:             data["uri"].(string),
+		Headers:          headers,
+		HttpMethod:       data["http_method"].(string),
+		RelativeUri:      data["relative_uri"].(string),
+		AppEngineRouting: expandAppEngineRouting(data["app_engine_routing"].([]interface{})),
+	}
+}
+
+func flattenAppEngineHttpTarget(appEngineHttpTarget *cloudscheduler.AppEngineHttpTarget) []map[string]interface{} {
+	result := make([]map[string]interface{}, 0, 1)
+	if appEngineHttpTarget == nil {
+		return nil
+	}
+
+	result = append(result, map[string]interface{}{
+		"headers":            appEngineHttpTarget.Headers,
+		"http_method":        appEngineHttpTarget.HttpMethod,
+		"relative_uri":       appEngineHttpTarget.RelativeUri,
+		"app_engine_routing": flattenAppEngineRouting(appEngineHttpTarget.AppEngineRouting),
+	})
+
+	return result
+}
+
+func expandHttpTarget(configured []interface{}) *cloudscheduler.HttpTarget {
+	if len(configured) == 0 || configured[0] == nil {
+		return &cloudscheduler.HttpTarget{}
+	}
+
+	data := configured[0].(map[string]interface{})
+
+	headers := make(map[string]string)
+	for k, val := range data["headers"].(map[string]interface{}) {
+		headers[k] = val.(string)
+	}
+	return &cloudscheduler.HttpTarget{
+		Body:       data["uri"].(string),
+		Headers:    headers,
+		HttpMethod: data["http_method"].(string),
+		Uri:        data["body"].(string),
+	}
+}
+
+func flattenHttpTarget(httpTarget *cloudscheduler.HttpTarget) []map[string]interface{} {
+	result := make([]map[string]interface{}, 0, 1)
+	if httpTarget == nil {
+		return nil
+	}
+
+	result = append(result, map[string]interface{}{
+		"uri":         httpTarget.Uri,
+		"headers":     httpTarget.Headers,
+		"http_method": httpTarget.HttpMethod,
+		"body":        httpTarget.Body,
+	})
+
+	return result
+}
+
+func expandRetryConfig(configured []interface{}) *cloudscheduler.RetryConfig {
+	if len(configured) == 0 || configured[0] == nil {
+		return &cloudscheduler.RetryConfig{}
+	}
+
+	data := configured[0].(map[string]interface{})
+	return &cloudscheduler.RetryConfig{
+		MaxBackoffDuration: data["max_backoff_duration"].(string),
+		MaxDoublings:       int64(data["max_doublings"].(int)),
+		MaxRetryDuration:   data["max_retry_duration"].(string),
+		MinBackoffDuration: data["min_backoff_duration"].(string),
+		RetryCount:         int64(data["retry_count"].(int)),
+	}
+}
+
+func flattenRetryConfig(retryConfig *cloudscheduler.RetryConfig) []map[string]interface{} {
+	result := make([]map[string]interface{}, 0, 1)
+	if retryConfig == nil {
+		return nil
+	}
+
+	result = append(result, map[string]interface{}{
+		"retry_count":          retryConfig.RetryCount,
+		"max_retry_duration":   retryConfig.MaxRetryDuration,
+		"min_backoff_duration": retryConfig.MinBackoffDuration,
+		"max_backoff_duration": retryConfig.MaxBackoffDuration,
+		"max_doublings":        retryConfig.MaxDoublings,
+	})
+
+	return result
+}
+
 func resourceCloudSchedulerJobCreate(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	project, err := getProject(d, config)
+	if err != nil {
+		return err
+	}
+
+	region, err := getRegion(d, config)
+	if err != nil {
+		return err
+	}
+
+	jobId := &jobId{
+		Project: project,
+		Region:  region,
+		Name:    d.Get("name").(string),
+	}
+
+	job := &cloudscheduler.Job{
+		Name:            jobId.jobId(),
+		Schedule:        d.Get("schedule").(string),
+		ForceSendFields: []string{},
+	}
+
+	if v, ok := d.GetOk("description"); ok {
+		job.Description = v.(string)
+	}
+
+	if v, ok := d.GetOk("pubsub_target"); ok {
+		job.PubsubTarget = expandPubsubTarget(v.([]interface{}))
+	} else if v, ok := d.GetOk("app_engine_http_target"); ok {
+		job.AppEngineHttpTarget = expandAppEngineHttpTarget(v.([]interface{}))
+	} else if v, ok := d.GetOk("http_target"); ok {
+		job.HttpTarget = expandHttpTarget(v.([]interface{}))
+	} else {
+		return fmt.Errorf("One of `pubsub_target`, `app_engine_http_target` or `http_target` is required: " +
+			"You must specify a target when deploying a new scheduler job.")
+	}
+
+	if v, ok := d.GetOk("time_zone"); ok {
+		job.TimeZone = v.(string)
+	}
+
+	if v, ok := d.GetOk("retry_config"); ok {
+		job.RetryConfig = expandRetryConfig(v.([]interface{}))
+	}
+
+	log.Printf("[DEBUG] Creating scheduler job: %s", job.Name)
+	op, err := config.clientCloudScheduler.Projects.Locations.Jobs.Create(
+		jobId.locationId(), job).Do()
+	_ = op
+	if err != nil {
+		return err
+	}
+
+	// Name of job should be unique
+	d.SetId(jobId.terraformId())
+
+	return resourceCloudSchedulerRead(d, meta)
+}
+
+func resourceCloudSchedulerRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	jobId, err := parseJobId(d.Id(), config)
+	if err != nil {
+		return err
+	}
+
+	job, err := config.clientCloudScheduler.Projects.Locations.Jobs.Get(jobId.jobId()).Do()
+	if err != nil {
+		return handleNotFoundError(err, d, fmt.Sprintf("Target CloudScheduler Job %q", jobId.Name))
+	}
+
+	d.Set("name", jobId.Name)
+	d.Set("description", job.Description)
+	d.Set("schedule", job.Schedule)
+
+	if job.PubsubTarget != nil {
+		d.Set("pubsub_target", flattenPubsubTarget(job.PubsubTarget))
+	} else if job.HttpTarget != nil {
+		d.Set("pubsub_target", flattenHttpTarget(job.HttpTarget))
+	} else if job.AppEngineHttpTarget != nil {
+		d.Set("pubsub_target", flattenAppEngineHttpTarget(job.AppEngineHttpTarget))
+	}
+
+	if job.RetryConfig != nil {
+		d.Set("retry_config", flattenRetryConfig(job.RetryConfig))
+	}
+
+	if job.PubsubTarget != nil {
+		d.Set("timezone", job.TimeZone)
+	}
+	d.Set("region", jobId.Region)
+	d.Set("project", jobId.Project)
+
 	return nil
 }
 
